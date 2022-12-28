25d089ad57bc282f5f6d27ff493e6cc8f142612f
Caleb Thomas <cjthomas00@comcast.net>
2022-12-28 09:53:13 -0700

    complete renter spec test file.

--- test/renter_spec.rb
+++ test/renter_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Renter do
   it "exists" do 
     expect(@renter).to be_an_instance_of(Renter)
   end
+
+  it "has attributes" do
+    expect(@renter.name).to eq("Patrick Star")
+    expect(@renter.credit_card_number).to eq("4242424242424242")
+  end
 end
